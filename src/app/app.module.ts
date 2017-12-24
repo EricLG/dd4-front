689120f3140af1ac121c70507bb4991d0bb78b86
Eric <eric.le.guellaut@hotmail.fr>
2017-12-24 15:42:54 +0100

    Gestion des liens avec les routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes  } from  '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MagicItemsComponent } from './items/magic/magic-items.component';
 
+export  const routes: Routes  = [
+    { path: '', component:  HomeComponent },  //  path: '/'
+    { path: 'magic-items',   component:  MagicItemsComponent },
+];
 
 @NgModule({
   declarations: [
@@ -15,6 +20,7 @@ import { MagicItemsComponent } from './items/magic/magic-items.component';
   ],
   imports: [
     NgbModule.forRoot(),
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
