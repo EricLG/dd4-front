2ea40b1a42860120b6ba07f14f91942dbf5b095c
Eric <eric.le.guellaut@hotmail.fr>
2017-12-25 16:07:02 +0100

    MagicItems appelle MagicItemDetail pour chaque item

--- src/app/items/magic/magic-items.component.ts
+++ src/app/items/magic/magic-items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MagicItem } from '../../model/magic-item'
 
 @Component({
   selector: 'app-magic-items',
@@ -8,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 
 export class MagicItemsComponent implements OnInit {
 
+  magicItems: MagicItem[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.getMagicItems();
+  }
+
+  getMagicItems(): void {
+    this.magicItems.push(
+      new MagicItem("Arme magique", "Lorem", "jet d'Attaque et jet de dégâts", "propriete", "pouvoir", "+1D6 par bonus d'altération", "1", "common", "MdJ1"),
+      new MagicItem("Arme de feu", "Lorem feu", "jet d'Attaque et jet de dégâts", "propriete", "pouvoir", "+1D6 par bonus d'altération", "2", "common", "MdJ2"),
+      new MagicItem("Arme de froid", "Lorem glace", "jet d'Attaque et jet de dégâts", "propriete", "pouvoir", "+1D6 par bonus d'altération", "3", "common", "MdJ3")
+      )
   }
 
 }
