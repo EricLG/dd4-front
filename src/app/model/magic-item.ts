86d89a682ef8243812aeb5536be8034e096951e4
Eric <eric.le.guellaut@hotmail.fr>
2017-12-25 13:47:35 +0100

    Ajout d'un model magicItem

---
+++ src/app/model/magic-item.ts
@@ -0,0 +1,14 @@
+export class MagicItem {
+
+  constructor(
+    public name?: string,
+    public description?: string,
+    public alteration?: string,
+    public property?: string,
+    public power?: string,
+    public critical?: string,
+    public level_min?: string,
+    public rarity?: string,
+    public source?: string
+  ){}
+}
