602de9a2215c0ab0cd15954e08b31adae2543b96
Eric <eric.le.guellaut@hotmail.fr>
2017-12-29 22:30:30 +0100

    Création model race

---
+++ src/app/model/race.ts
@@ -0,0 +1,22 @@
+import { deburr, toLower, replace } from 'lodash'
+
+export class Race {
+
+  constructor(
+	public name?: string,
+	public average_height?: string,
+	public average_weight?: string,
+	public cat_size?: string,
+	public speed?: string,
+	public vision?: string,
+	public language?: string,
+	public nb_language_sup?: string,
+	public information?: string,
+	public source?: string,
+  ){}
+
+  format_name() {
+  	return replace(deburr(toLower(this.name)), ' ', '_')
+
+  }
+}
